15a37b1c2d64dcfaabb10ecf2bd39d4600cdbfdd
Christoph Berger <christophberger@users.noreply.github.com>
2016-06-07 16:03:52 +0200

    Code runs, text is in progress

--- di.go
+++ di.go
@@ -15,37 +15,102 @@ author = "Christoph Berger"
 email = "chris@appliedgo.net"
 date = "2016-00-00"
 publishdate = "2016-00-00"
-domains = [""]
-tags = ["", "", ""]
+domains = ["Architecture"]
+tags = ["Dependency Injection", "Separation Of Concerns", "Interface"]
 categories = ["Tutorial"]
 +++
 
-### Summary goes here
+Layered software architectures adhere to the Dependency Rule: Source code dependencies can only point from lower-level layers to higher-level layers, and never vice versa. But at some point you need to connect the lower layers to the higher layers without violating the rule. Here is how.
 
 <!--more-->
 
-## Intro goes here
+## Software Architecture
+
+The nightmare of every software maintainer: After fixing a bug in a small utility function from the PDF formatter module, database queries start returning wrong results. After long and laborious search, it turns out that the database code uses the PDF utility function for formatting some strings.
+
+Sounds familiar?
+
+Bugs of this kind have inspired software engineers to think up ways of strucring software that effectively prohibits uncontrolled proliferation of mutual dependencies between the entities of a software system. The result is called a software architecture.
+
+Many different [architecture styles and patterns](https://en.wikipedia.org/wiki/List_of_software_architecture_styles_and_patterns) have evolved over time, at different abstraction levels and with different levels of complexity. Among them, layered architectures seem to represent a fairly versatile concept, applicable to a large range of scenarios.
+
+
+## The Clean Architecture
+
+An excellent summary of the principles of good layered architectures is [The Clean Architecture](http://blog.8thlight.com/uncle-bob/2012/08/13/the-clean-architecture.html), a blog article about the architecture of the same name. The Clean Architecture model consists of four layers (or more, if you need it) of increasing abstraction.
+
+![The Clean Architecture](cleanarchitecture.png)
+
+The layers are laid out as circles. The innermost circle
+
+At the center of any software architecture is the separation of concerns.
 
 ## The code
+
 */
 
 // ## Imports and globals
 package main
 
-import "math"
-
-var (
-	l1, l2, a1, a2, b1, b2, d float64
+import (
+	"fmt"
+	"strings"
 )
 
-func beta1(x, y float64) float64 {
-	return math.Atan2(y, x)
+type Speaker interface {
+	Speak(string)
 }
 
-func beta2(d, l1, l2 float64) float64 {
-	return (d*d + l2*l2 - l1*l1) / (2 * l2 * d)
+type Announcement struct {
+	message string
+	speaker Speaker
 }
 
-func alpha2(d, l1, l2 float64) float64 {
-	return (d*d - l2*l2 + l1*l1) / (2 * d * l1)
+func NewAnnouncement(m string, s Speaker) *Announcement {
+	return &Announcement{m, s}
+}
+
+func (a *Announcement) SetSpeaker(s Speaker) {
+	a.speaker = s
+}
+func (a *Announcement) Announce() {
+	a.speaker.Speak(a.message)
+}
+
+type Newscaster struct{}
+
+func (n *Newscaster) Speak(msg string) {
+	fmt.Println("Breaking news:")
+	fmt.Println(msg)
+	fmt.Println()
+}
+
+type Preacher struct{}
+
+func (p *Preacher) Speak(msg string) {
+	fmt.Println("And so the prophets say:")
+	fmt.Println(msg)
+	fmt.Println()
+}
+
+type SalesPromoter struct{}
+
+func (s *SalesPromoter) Speak(msg string) {
+	fmt.Println(strings.ToUpper(msg), "!!! BUY NOW!!!")
+	fmt.Println()
+}
+
+func main() {
+	n := &Newscaster{}
+	p := &Preacher{}
+	s := &SalesPromoter{}
+
+	a := NewAnnouncement("Gophers are incredibly smart", n)
+	a.Announce()
+
+	a.SetSpeaker(p)
+	a.Announce()
+
+	a.SetSpeaker(s)
+	a.Announce()
 }
